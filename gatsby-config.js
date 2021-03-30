6beffabd6891c9289a125d9db675f5e8d2f5b17d
RyunosukeOguri <amagurik2@gmail.com>
2021-03-31 01:18:18 +0900

    add: GraphQL file page

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,5 +42,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,58 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import * as aboutStyles from "./about.module.scss"
 
-const AboutPage = () => (
-  <Layout>
-    <SEO title="Page about" />
-    <div className={aboutStyles.overview}>
-      <h1>Hi from the about page</h1>
-      <p>Welcome to about</p>
-    </div>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const AboutPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Page about" />
+      <div className={aboutStyles.overview}>
+        <h1>My Site's Files</h1>
+        <p>GraphQLクエリ</p>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
 
 export default AboutPage
